d0763c1dd271db86e64c00eab83172e9eb099ab3
ketantb <kbhatkhore777@gmail.com>
2023-04-10 03:27:24 +0530

    removed useless assets

--- frontend/src/Components/why-choose-us.js
+++ frontend/src/Components/why-choose-us.js
@@ -22,20 +22,21 @@ const WhyChooseUs = () => {
                     <div>
                         <p class="why-choose-us-dummy-bullets">2</p>
                         <div>
-                            <h4>Process Enablement</h4>
+                            <h4>We Innovate for You</h4>
                             <p>
-                                We work very closely with clients to provide consulting, maintaining,
-                                and facilitating process re-engineering as required to enable client for success in this changing world.
+                                We're a team of dreamers, thinkers and creators. We're constabtly evolving our products to
+                                reach even higher standards for design, quality and production.
                             </p>
                         </div>
                     </div>
                     <div>
                         <p class="why-choose-us-dummy-bullets">3</p>
                         <div>
-                            <h4>Process Enablement</h4>
+                            <h4>Our clients are happy</h4>
                             <p>
-                                We work very closely with clients to provide consulting, maintaining,
-                                and facilitating process re-engineering as required to enable client for success in this changing world.
+                                We've developed and refined our methods through past experirnces,
+                                Nothing gives us greater pride than seeing our clients delighted with
+                                the work we've done together our case studies are solid proof.
                             </p>
                         </div>
                     </div>
